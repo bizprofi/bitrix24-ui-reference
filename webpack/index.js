a331c916116c56a6a00994be678e182796e20f6d
Igor Denisenko <im.denisenko@bizprofi.ru>
2019-04-01 23:01:03 +0300

    Fix of ui.notification

--- webpack/index.js
+++ webpack/index.js
@@ -1,3 +1,4 @@
+require(/* webpackChunkName: 'ui.alerts' */'../../public_html/bitrix/js/main/core/core.js');
 import(/* webpackChunkName: 'ui.alerts' */'../../public_html/bitrix/js/ui/alerts/ui.alerts.css');
 import(/* webpackChunkName: 'ui.fonts' */'../../public_html/bitrix/js/ui/fonts/opensans/ui.font.opensans.css');
 import(/* webpackChunkName: 'ui.buttons' */'../../public_html/bitrix/js/ui/buttons/ui.buttons.css');
@@ -5,4 +6,7 @@ import(/* webpackChunkName: 'ui.buttons' */'../../public_html/bitrix/js/ui/butto
 import(/* webpackChunkName: 'ui.buttons.icons' */'../../public_html/bitrix/js/ui/buttons/icons/ui.buttons.icons.css');
 import(/* webpackChunkName: 'ui.buttons.icons' */'../../public_html/bitrix/js/ui/buttons/icons/ui.buttons.icons.ie.css');
 import(/* webpackChunkName: 'ui.icons' */'../../public_html/bitrix/js/ui/icons/ui.icons.css');
+import(/* webpackChunkName: 'ui.notification' */'../../public_html/bitrix/js/ui/notification/ui.notification.balloon.js');
+import(/* webpackChunkName: 'ui.notification' */'../../public_html/bitrix/js/ui/notification/ui.notification.stack.js');
+import(/* webpackChunkName: 'ui.notification' */'../../public_html/bitrix/js/ui/notification/ui.notification.center.js');
 import(/* webpackChunkName: 'ui.notification' */'../../public_html/bitrix/js/ui/notification/ui.notification.css');
